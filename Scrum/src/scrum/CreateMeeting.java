5f9b4c6facb8e9d1844bdd642ebcf81a9ac0cd1e
marvinjac <56878406+marvinjac@users.noreply.github.com>
2020-04-15 09:39:57 +0200

    SKAPAMÖTFORTS

--- Scrum/src/scrum/CreateMeeting.java
+++ Scrum/src/scrum/CreateMeeting.java
@@ -4,7 +4,11 @@
  * and open the template in the editor.
  */
 package scrum;
+
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import javax.swing.JOptionPane;
 import javax.swing.ListModel;
 import oru.inf.InfDB;
@@ -18,60 +22,69 @@ public class CreateMeeting extends javax.swing.JFrame {
 
     private InfDB idb;
     private int currentUser;
+
     /**
      * Creates new form CreateMeeting
      */
     public CreateMeeting(InfDB idb, int id) {
         initComponents();
         this.idb = idb;
-        this.currentUser = id; 
+        this.currentUser = id;
         FillReciver();
     }
 
-
-  public void CreateMeeting() {
-      try{
+    public void CreateMeeting() {
+        try {
             int creator = currentUser;
             String description = tfDescription.getText();
             String date = tfDate.getText();
             String time = tfTime.getText();
             String room = tfRoom.getText();
-            String sql = "insert into MEETING values("+idb.getAutoIncrement("MEETING","MEETING_ID")+","+creator+",'"+description+"','"+date+"','"+time+"','"+room+"')";
-            
+            String mID = idb.getAutoIncrement("MEETING", "MEETING_ID");
+            String sql = "insert into MEETING values(" + idb.getAutoIncrement("MEETING", "MEETING_ID") + "," + creator + ",'" + description + "','" + date + "','" + time + "','" + room + "')";
             idb.insert(sql);
+           
+            String reciver = taReciver.getText();
+            String[] splited = reciver.split("\\s+");
+                for(int i=0;i<splited.length;i++){
+                    String sqlQ = "SELECT USER_ID FROM USER1 WHERE EMAIL ='" +splited[i]+"'";
+                    String reciverID = idb.fetchSingle(sqlQ);
+                    System.out.println(reciverID);
+                    String sqlInsert = "INSERT INTO MEETINGREQUEST VALUES ("+mID+","+reciverID+", 1)";
+                    idb.insert(sqlInsert);
+                    System.out.println((i+1)+"."+splited[i]);
+                }
+                
             JOptionPane.showMessageDialog(null, "Mötet har lagts till");
-            }
-      catch(InfException e){
             
-            System.out.println(e.getMessage());    
-            }
-  
-  }
-  
-  public void FillReciver() {
-      cbReciver.removeAllItems();
-      String sqlFraga = "SELECT EMAIL from User1";
-        
+        } catch (InfException e) {
+
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void FillReciver() {
+        cbReciver.removeAllItems();
+        String sqlFraga = "SELECT EMAIL from User1";
+
         try {
             ArrayList<String> names = idb.fetchColumn(sqlFraga);
-            
-            if(names != null) {
-                for(String n : names) {
+
+            if (names != null) {
+                for (String n : names) {
                     cbReciver.addItem(n);
                 }
             }
-        }
-        catch(InfException e) {
+        } catch (InfException e) {
             System.out.println(e.getMessage());
         }
-  }
-  
-  public void MeetingRequest() {
-      
-      
-      
-  }
-  
+    }
+
+    public void MeetingRequest() {
+
+    }
+
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -134,7 +147,7 @@ public class CreateMeeting extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(24, 24, 24)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -155,7 +168,7 @@ public class CreateMeeting extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(cbReciver, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 239, Short.MAX_VALUE)))
                 .addContainerGap(207, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -182,21 +195,18 @@ public class CreateMeeting extends javax.swing.JFrame {
                 .addComponent(lblReciever)
                 .addGap(11, 11, 11)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(cbReciver, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 123, Short.MAX_VALUE)
-                        .addComponent(btnCreateMeeting)
-                        .addGap(72, 72, 72))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                    .addComponent(cbReciver, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnCreateMeeting)
+                .addGap(72, 72, 72))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnCreateMeetingActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCreateMeetingActionPerformed
-          CreateMeeting();
+        CreateMeeting();
     }//GEN-LAST:event_btnCreateMeetingActionPerformed
 
     private void cbReciverMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_cbReciverMouseClicked
@@ -204,19 +214,17 @@ public class CreateMeeting extends javax.swing.JFrame {
         String selectedName;
         selectedName = (String) cbReciver.getSelectedItem();
         String texten = taReciver.getText().trim();
-        
-        if(!texten.equals("")) {
+
+        if (!texten.equals("")) {
             taReciver.append("\n");
             taReciver.append(selectedName);
             System.out.println("HEj2");
-        }
-        else {
+        } else {
             taReciver.append(selectedName);
             System.out.println("HEj1");
         }
     }//GEN-LAST:event_cbReciverMouseClicked
 
-    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCreateMeeting;
