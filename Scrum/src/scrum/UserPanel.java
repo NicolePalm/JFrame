a54d385af2c4dd913f804c733db309eb6a149844
jomill99 <62940234+jomill99@users.noreply.github.com>
2020-04-17 09:30:07 +0200

    Andra kategoriboxen - user panel

--- Scrum/src/scrum/UserPanel.java
+++ Scrum/src/scrum/UserPanel.java
@@ -25,7 +25,7 @@ public class UserPanel extends javax.swing.JFrame {
         this.idb = idb;
         this.currentUser = id;
         initComponents();
-        post();
+       uppdateraUnderKategori();
     }
     
     
@@ -36,10 +36,17 @@ public class UserPanel extends javax.swing.JFrame {
             int categoryId;
 
             System.out.println(category);
+ 
+            String valt = jUnderKategori.getSelectedItem().toString();
+            String fraga = "select category_id from category where categoryname = '" + valt + "'";
+            String svaret; 
+            svaret = idb.fetchSingle(fraga);
+            int sver = Integer.parseInt(svaret);
 
-
+            System.out.println(1);
             String postQuery = "SELECT post_id, title, postdate, posttime, post.category_id FROM post, category \n" +
-                                "WHERE post.CATEGORY_ID = category.CATEGORY_ID AND categorytype = '" + category + "' ORDER BY postdate DESC, posttime DESC";
+                                "WHERE post.CATEGORY_ID = category.CATEGORY_ID AND categorytype = '" + category + "' and post.CATEGORY_ID = " + sver + " ORDER BY postdate DESC, posttime DESC";
+           
             ArrayList<HashMap<String, String>> posts = idb.fetchRows(postQuery);
             String queryId;
             String queryTitle;
@@ -72,7 +79,7 @@ public class UserPanel extends javax.swing.JFrame {
 
         catch (Exception e) {
             System.out.println("Error");
-
+JOptionPane.showMessageDialog(null, "Oops!\nSer ut som att det inte finns inlägg under denna kategori!");
         }
     }
 
@@ -89,6 +96,8 @@ public class UserPanel extends javax.swing.JFrame {
         postList = new javax.swing.JList<>();
         categoryCbx = new javax.swing.JComboBox<>();
         btnCreateMeeting = new javax.swing.JButton();
+        jUnderKategori = new javax.swing.JComboBox<>();
+        jButton1 = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
@@ -107,11 +116,6 @@ public class UserPanel extends javax.swing.JFrame {
         });
 
         postList.setFont(new java.awt.Font("Verdana", 0, 24)); // NOI18N
-        postList.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
         postList.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         postList.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
@@ -139,6 +143,28 @@ public class UserPanel extends javax.swing.JFrame {
             }
         });
 
+        jUnderKategori.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jUnderKategori.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jUnderKategoriMouseClicked(evt);
+            }
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jUnderKategoriMouseReleased(evt);
+            }
+        });
+        jUnderKategori.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jUnderKategoriActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("Uppdatera");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         jMenu1.setText("File");
         jMenuBar1.add(jMenu1);
 
@@ -152,34 +178,43 @@ public class UserPanel extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGap(33, 33, 33)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(335, 335, 335)
-                        .addComponent(jLabel1))
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jUnderKategori, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(27, 27, 27)
+                        .addComponent(categoryCbx, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 729, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(33, 33, 33)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel2)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(categoryCbx, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 729, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jNewPost)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(btnCreateMeeting, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(191, 191, 191)))))
+                        .addComponent(jNewPost)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnCreateMeeting, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(191, 191, 191)))
                 .addContainerGap(37, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(335, 335, 335)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(72, 72, 72))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
-                .addGap(21, 21, 21)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(21, 21, 21))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(categoryCbx, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(categoryCbx, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jUnderKategori, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 452, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -197,7 +232,7 @@ public class UserPanel extends javax.swing.JFrame {
     }//GEN-LAST:event_jNewPostActionPerformed
 
     private void categoryCbxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_categoryCbxActionPerformed
-        post();
+uppdateraUnderKategori();       
     }//GEN-LAST:event_categoryCbxActionPerformed
 
     private void postListValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_postListValueChanged
@@ -228,6 +263,15 @@ public class UserPanel extends javax.swing.JFrame {
     }//GEN-LAST:event_postListValueChanged
 
 
+                                        
+
+    
+    
+    public void uppdateraUnderKategori(){
+     jUnderKategori.removeAllItems();
+        String type = categoryCbx.getSelectedItem().toString();
+        new FillComboBoxFromDb(idb).fillComboboxCategories(jUnderKategori, type);       
+    }
     
     
     
@@ -239,6 +283,23 @@ public class UserPanel extends javax.swing.JFrame {
         new CreateMeeting(idb,currentUser).setVisible(true);
     }//GEN-LAST:event_btnCreateMeetingActionPerformed
 
+    private void jUnderKategoriActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jUnderKategoriActionPerformed
+         // TODO add your handling code here:
+    }//GEN-LAST:event_jUnderKategoriActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+post(); 
+// TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jUnderKategoriMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jUnderKategoriMouseReleased
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jUnderKategoriMouseReleased
+
+    private void jUnderKategoriMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jUnderKategoriMouseClicked
+post();        // TODO add your handling code here:
+    }//GEN-LAST:event_jUnderKategoriMouseClicked
+ 
     /**
      * @param args the command line arguments
      */
@@ -248,6 +309,7 @@ public class UserPanel extends javax.swing.JFrame {
     private javax.swing.JButton btnCreateMeeting;
     private javax.swing.JComboBox<String> categoryCbx;
     private org.jdatepicker.impl.DateComponentFormatter dateComponentFormatter1;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JMenu jMenu1;
@@ -255,6 +317,7 @@ public class UserPanel extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JButton jNewPost;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JComboBox<String> jUnderKategori;
     private javax.swing.JList<String> postList;
     // End of variables declaration//GEN-END:variables
 }
