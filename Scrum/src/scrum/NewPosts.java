28fca47feb377429b8b10a88538a5b6f4629feef
DanielBredberg <56878344+DanielBredberg@users.noreply.github.com>
2020-04-20 18:45:39 +0200

    123

--- Scrum/src/scrum/NewPosts.java
+++ Scrum/src/scrum/NewPosts.java
@@ -45,6 +45,7 @@ public class NewPosts extends javax.swing.JFrame {
         jFile = new javax.swing.JButton();
         jFileName = new javax.swing.JLabel();
         jHome = new javax.swing.JButton();
+        jSetPublish = new javax.swing.JCheckBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -95,6 +96,13 @@ public class NewPosts extends javax.swing.JFrame {
             }
         });
 
+        jSetPublish.setText("Publish");
+        jSetPublish.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jSetPublishActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -107,18 +115,20 @@ public class NewPosts extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jlblTitle)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 455, Short.MAX_VALUE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jFile)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jCreatePost))
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(jFile)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(jCreatePost))
                                     .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jTitle, javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(layout.createSequentialGroup()
                                         .addComponent(jType, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addGap(37, 37, 37)
                                         .addComponent(jCategories, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(jSetPublish)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                         .addComponent(jNewCategory)))
                                 .addGap(12, 12, 12)))
@@ -145,7 +155,8 @@ public class NewPosts extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jCategories, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jType, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jNewCategory))
+                    .addComponent(jNewCategory)
+                    .addComponent(jSetPublish))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 360, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(23, 23, 23)
@@ -162,9 +173,20 @@ public class NewPosts extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jTitleActionPerformed
 
+    private int GetCheckBoxStatus(){
+    boolean boxStatus = jSetPublish.isSelected();
+    int boxStatusInt = 0;
+    if(boxStatus = true){
+       boxStatusInt = 1;
+    }
+    return boxStatusInt;
+    }
+    
     private void jCreatePostActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCreatePostActionPerformed
         String title = jTitle.getText();
         String content = jText.getText();
+        int publishStatus = GetCheckBoxStatus();
+        
         if(title.isEmpty() || content.isEmpty()){
             JOptionPane.showMessageDialog(null, "Fyll i både titel och text!");
             jTitle.requestFocus();
@@ -181,7 +203,7 @@ public class NewPosts extends javax.swing.JFrame {
             String id = idb.getAutoIncrement("POST","POST_ID");
             
             if(currentFile == null){
-                String insertPost = "INSERT INTO POST (POST_ID,TITLE,CONTENT,SEARCHPATH,POSTDATE,POSTTIME,POSTER_ID,CATEGORY_ID) VALUES ("+id+",'"+title+"','"+content+"','default','"+currentDate+"','"+tiden+"',"+currentUser+","+categoryID+")";
+                String insertPost = "INSERT INTO POST (POST_ID,TITLE,CONTENT,SEARCHPATH,POSTDATE,POSTTIME,POSTER_ID,CATEGORY_ID,POSTSTATUS) VALUES ("+id+",'"+title+"','"+content+"','default','"+currentDate+"','"+tiden+"',"+currentUser+","+categoryID+","+publishStatus+")";
                 idb.insert(insertPost);
             }
             else{
@@ -227,9 +249,14 @@ public class NewPosts extends javax.swing.JFrame {
 
         dispose();
     }//GEN-LAST:event_jHomeActionPerformed
+
+    private void jSetPublishActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jSetPublishActionPerformed
+        
+    }//GEN-LAST:event_jSetPublishActionPerformed
     
     private String GetCurrentFile() 
     {
+        
         
         String fileName = currentFile.getAbsolutePath();
         this.currentExt = HandleFiles.getExtensionByApacheCommonLib(fileName);
@@ -262,6 +289,7 @@ public class NewPosts extends javax.swing.JFrame {
     private javax.swing.JButton jHome;
     private javax.swing.JButton jNewCategory;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JCheckBox jSetPublish;
     private javax.swing.JTextArea jText;
     private javax.swing.JTextField jTitle;
     private javax.swing.JComboBox<String> jType;
